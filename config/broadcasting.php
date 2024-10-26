33aaf14528cfd5f74155b9beba0b48127df6adaa
Sajid <md.sajidulislam@outlook.com>
2024-10-26 15:47:03 +0600

    fixing

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => false, // Change to true if you're using HTTPS
-                'host' => '127.0.0.1',
+                'host' => env('PUSHER_APP_ID', 'localhost'),
                 'port' => 6001,
                 'scheme' => 'http',
             ],
